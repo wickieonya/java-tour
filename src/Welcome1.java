f88257479ee19384eb730163f6d3abef324d019c
wickieonya <wickieonya@gmail.com>
2021-11-25 12:25:38 +0300

    initial java material audit

---
+++ src/Welcome1.java
@@ -0,0 +1,8 @@
+
+// printing a line of text with multiple statements
+public class Welcome1 {
+    public static void main(String[] args) {
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+    } // end method main
+} // end class Welcome1
